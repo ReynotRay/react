6aa3ae5e2e9ee52f64f1cb0174f4c09240f40370
ReynotRay <reygarcia718@gmail.com>
2016-09-27 22:12:33 -0600

    ex 5 304

--- js/clientapp.jsx
+++ js/clientapp.jsx
@@ -2,17 +2,14 @@ var React = require('react')
 var ReactDOM = require ('react-dom')
 var MyTitle = require('./MyTitle')
 
-var div = React.DOM.div
-
-var MyTitleFact = React.createFactory(MyTitle)
-var ce = React.createElement
-
-var MyFirstComponent = (
-	div(null,
+var MyFirstComponent = function () {
+	<div>
 		MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
 		React.createElement(MyTitle,{title: 'use props everywhere!', color: 'mediumaquamarine'}),
 		ce(MyTitle,{title: 'Props are the best!', color: 'papayawhip'})
-	)
-)
+	
+	</div>
+		)
+}
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'))
